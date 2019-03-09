5002d3af03d2224e382ca23021bf30eb25c3aa47
Chandan Rauniyar <chandan.rauniyar@myob.com>
2019-03-09 19:37:05 +1100

    Added screens, updated bottom tab navigator

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, View, Text } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
-
+import {
+  Ionicons
+} from '@expo/vector-icons';
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import TrendingScreen from '../screens/TrendingScreen';
+import SubscriptionsScreen from '../screens/SubscriptionsScreen';
+import InboxScreen from '../screens/InboxScreen';
+import LibraryScreen from '../screens/LibraryScreen';
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
@@ -13,48 +17,108 @@ const HomeStack = createStackNavigator({
 
 HomeStack.navigationOptions = {
   tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
+  tabBarIcon: ({ tintColor, focused }) => (
     <TabBarIcon
       focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
+      name={'ios-home'} size={20} style={{ color: tintColor}}
     />
   ),
 };
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
+const TrendingStack = createStackNavigator({
+  Trending: TrendingScreen,
 });
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
+TrendingStack.navigationOptions = {
+  tabBarLabel: 'Trending',
+  tabBarIcon: ({ tintColor, focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+      name={'ios-flame'} size={20} style={{ color: tintColor}}
     />
   ),
 };
 
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
+const SubscriptionsStack = createStackNavigator({
+  Subscriptions: SubscriptionsScreen,
 });
 
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
+SubscriptionsStack.navigationOptions = {
+  tabBarLabel: 'Subscriptions',
+  tabBarIcon: ({ tintColor, focused }) => (
+    <View style={{ width: 24, height: 24, margin: 5 }}>
+            <TabBarIcon
+              focused={focused}
+              name={'ios-play-circle'} size={20} style={{ color: tintColor}}
+            />
+          <View
+            style={{
+              // /If you're using react-native < 0.57 overflow outside of the parent
+              // will not work on Android, see https://git.io/fhLJ8
+              position: 'absolute',
+              right: 2,
+              top: 1,
+              backgroundColor: 'red',
+              borderRadius: 6,
+              width: 8,
+              height: 8,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+              
+            </Text>
+          </View>
+        </View>
+  ),
+};
+
+const InboxStack = createStackNavigator({
+  Inbox: InboxScreen,
+});
+
+InboxStack.navigationOptions = {
+  tabBarLabel: 'Inbox',
+  tabBarIcon: ({ tintColor, focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+      name={'ios-mail'} size={20} style={{ color: tintColor}}
+    />
+  ),
+};
+
+const LibraryStack = createStackNavigator({
+  Library: LibraryScreen,
+});
+
+LibraryStack.navigationOptions = {
+  tabBarLabel: 'Library',
+  tabBarIcon: ({ tintColor, focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={'ios-folder'} size={20} style={{ color: tintColor}}
     />
   ),
 };
 
 export default createBottomTabNavigator({
   HomeStack,
-  LinksStack,
-  SettingsStack,
+  TrendingStack,
+  SubscriptionsStack,
+  InboxStack,
+  LibraryStack
+}, {
+      tabBarOptions: {
+      activeTintColor: 'white',
+      inactiveTintColor: 'grey',
+      style: {
+        backgroundColor: '#121212',
+        borderTopWidth: 0,
+        shadowOffset: { width: 5, height: 3 },
+        shadowColor: 'black',
+        shadowOpacity: 0.5,
+        elevation: 5,
+        paddingVertical: 5
+      },
+    },
 });
