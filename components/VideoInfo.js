3c807da4ecd9e8e4dbb5784fac95d74bc63f06b8
Chandan Rauniyar <chandan.rauniyar@myob.com>
2019-03-10 21:39:10 +1100

    Removed boilerplate code

--- components/VideoInfo.js
+++ components/VideoInfo.js
@@ -16,13 +16,18 @@ export default class VideoInfo extends Component {
       <View style={styles.avatarContainer}>
        <Image style={styles.avatarImage} source={{uri: this.props.channelAvatarImage}} />
       </View>
-
-      <View style={styles.titleTextContainer}>
+      <View style={styles.textContainer}>
         <Text style={styles.titleText}>
           {this.props.videoTitle}
         </Text>
         <Text style={styles.descriptionText}>
-          {this.props.channelName} {this.props.videoInfo.description.views} {this.props.videoInfo.description.uploadDate}
+          {
+            this.props.channelName
+          } · {
+            this.props.videoInfo.description.views
+          } · {
+            this.props.videoInfo.description.uploadDate
+          }
         </Text>
       </View>
       <View>
@@ -42,9 +47,8 @@ const styles = StyleSheet.create({
         marginTop: 5,
       },
     avatarContainer: {
-      marginHorizontal: 15,
+      marginHorizontal: 10,
     },
-
     avatarImage: {
         width: 40,
         height: 40,
@@ -55,6 +59,10 @@ const styles = StyleSheet.create({
       fontWeight: '600',
         fontSize: 18,
         color: '#fff',
+    },
+    textContainer:{
+       paddingHorizontal: 15,
+       paddingBottom: 15,
     },
     titleText: {
         fontWeight: '600',
